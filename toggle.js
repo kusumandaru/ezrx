c2eb7f92570e996d22ea8521661d79b9404009e0
ndaru <angga.kusumandaru@gmail.com>
2017-03-04 13:17:52 +0700

    initial commit

---
+++ toggle.js
@@ -0,0 +1,31 @@
+$(function() {
+    var $css =$('link[rel~="js-ezrx.css"]'), toggle = false;
+
+    $.getJSON("ezrx-config.json", function(json) {
+        console.log('js and css : '+json.active); // this will show the info it in firebug console
+        activate(json.active, $css);
+    })
+
+    $('button').click(function() {
+        activate(toggle, $css);
+        toggle = !toggle;
+    });
+});
+
+function activate(toggle, $css) {
+    if (toggle) {
+        $css.prop('disabled', true);
+    } else {
+        $css.prop('disabled', false);
+        loadEzrx();
+    }
+}
+
+function loadEzrx()
+{
+    var runSecond = document.createElement("script");
+    runSecond.setAttribute("src", "js-ezrx.js");
+    document.body.appendChild(runSecond);
+}
+
+document.write("<script type='application/x-suppress'>");
